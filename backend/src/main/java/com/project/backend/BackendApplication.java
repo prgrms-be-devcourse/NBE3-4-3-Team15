4e34c2ede8ba4d6bcbbd1b42088fc2206cecb70c
정재익 <wodlr1207@naver.com>
2025-01-24 23:21:25 +0900

    Update BackendApplication.java

--- backend/src/main/java/com/project/backend/BackendApplication.java
+++ backend/src/main/java/com/project/backend/BackendApplication.java
@@ -2,7 +2,6 @@ package com.project.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class BackendApplication {
