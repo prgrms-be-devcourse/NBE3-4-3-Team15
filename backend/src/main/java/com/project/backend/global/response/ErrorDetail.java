d3508df5f34e055843c9e86abc7064f1d77be3e5
wjwj910 <wjwj910@gmail.com>
2025-01-27 15:41:45 +0900

    feat: ErrorDetail 추가

---
+++ backend/src/main/java/com/project/backend/global/response/ErrorDetail.java
@@ -0,0 +1,15 @@
+package com.project.backend.global.response;
+
+/**
+ * ErrorDetail
+ * 에러 발생 시 클라이언트에게 전달할 에러 상세 정보를 담는 클래스
+ * @param field 에러가 발생한 필드
+ * @param reason 에러 발생 원인
+ * author 이원재
+ * since 2021.01.27
+ */
+public record ErrorDetail(String field, String reason) {
+    public static ErrorDetail of(String field, String reason) {
+        return new ErrorDetail(field, reason);
+    }
+}
