828650f289742c65063d0e1b0f2dab6aa4cd2b7f
wjwj910 <wjwj910@gmail.com>
2025-01-27 15:10:29 +0900

    feat: GlobalControllerAdvice 추가

---
+++ backend/src/main/java/com/project/backend/global/GlobalControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.project.backend.global;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * GlobalControllerAdvice
+ * 애플리케이션 전역에서 발생하는 예외를 처리하는 클래스
+ * author 이원재
+ * since 2025.01.27
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalControllerAdvice {
+
+
+}
