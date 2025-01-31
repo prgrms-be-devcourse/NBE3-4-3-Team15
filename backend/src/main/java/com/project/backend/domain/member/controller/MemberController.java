780405d44f6a09d8ab7a73b6f06f6f22ce5beb1e
son <handsun000@gmail.com>
2025-01-31 16:30:33 +0900

    refactor: record -> Dto

--- backend/src/main/java/com/project/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/project/backend/domain/member/controller/MemberController.java
@@ -1,20 +1,18 @@
 package com.project.backend.domain.member.controller;
 
+import com.project.backend.domain.member.dto.LoginDto;
 import com.project.backend.domain.member.dto.MemberDto;
+import com.project.backend.domain.member.dto.MineDto;
+import com.project.backend.domain.member.dto.PasswordDto;
 import com.project.backend.domain.member.entity.Member;
 import com.project.backend.domain.member.exception.MemberException;
 import com.project.backend.domain.member.service.MemberService;
 import com.project.backend.global.response.GenericResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 
 import static com.project.backend.domain.member.exception.MemberErrorCode.*;
 
@@ -51,35 +49,19 @@ public class MemberController {
     }
 
     /**
-     * 로그인 요청 레코드
-     *
-     * @param id
-     * @param password
-     * @author 손진영
-     * @since 2025.01.27
-     */
-    record LoginReqBody(
-            @NotBlank
-            String id,
-            @NotBlank
-            String password
-    ) {
-    }
-
-    /**
      * 로그인 요청
      *
-     * @param reqBody
+     * @param loginDto
      * @return GenericResponse<MemberDto>
      * @author 손진영
      * @since 2025.01.27
      */
     @PostMapping("/login")
-    public GenericResponse<MemberDto> login(@RequestBody @Valid LoginReqBody reqBody) {
-        Member member = memberService.getMember(reqBody.id)
+    public GenericResponse<MemberDto> login(@RequestBody @Valid LoginDto loginDto) {
+        Member member = memberService.getMember(loginDto.getId())
                 .orElseThrow(() -> new MemberException(NON_EXISTING_ID));
 
-        if (!member.getPassword().equals(reqBody.password))
+        if (!member.getPassword().equals(loginDto.getPassword()))
             throw new MemberException(INCORRECT_PASSWORD);
 
         return GenericResponse.of(
@@ -107,34 +89,9 @@ public class MemberController {
     }
 
     /**
-     * 회원 정보 수정 레코드
-     *
-     * @param password
-     * @param email
-     * @param gender
-     * @param nickname
-     * @param birth
-     * @author 손진영
-     * @since 2025.01.28
-     */
-    record MineReqBody(
-            String password,
-            @NotBlank
-            @Length(max = 25)
-            @Email
-            String email,
-            int gender,
-            @NotBlank
-            @Length(min = 2, max = 20)
-            String nickname,
-            LocalDate birth
-    ) {
-    }
-
-    /**
      * 회원 정보 수정
      *
-     * @param reqBody
+     * @param mineDto
      * @return GenericResponse<MemberDto>
      * @Valid
      * @author 손진영
@@ -142,11 +99,16 @@ public class MemberController {
      */
     @PutMapping("/mine")
     @Transactional
-    public GenericResponse<MemberDto> mine(@RequestBody @Valid MineReqBody reqBody) {
+    public GenericResponse<MemberDto> mine(@RequestBody @Valid MineDto mineDto) {
 
         Member member = checkAuthMember();
 
-        memberService.modify(member, reqBody.password, reqBody.email, reqBody.gender, reqBody.nickname, reqBody.birth);
+        memberService.modify(member,
+                mineDto.getPassword(),
+                mineDto.getEmail(),
+                mineDto.getGender(),
+                mineDto.getNickname(),
+                mineDto.getBirth());
 
         return GenericResponse.of(
                 new MemberDto(member),
@@ -155,31 +117,19 @@ public class MemberController {
     }
 
     /**
-     * 회원 탈퇴 레코드
-     *
-     * @param password
-     * @author 손진영
-     * @since 2025.01.28
-     */
-    record QuitReqBody(
-         @NotBlank
-         String password
-    ){}
-
-    /**
      * 회원 탈퇴
      *
-     * @param reqBody
+     * @param passwordDto
      * @return GenericResponse
      * @Valid
      * @author 손진영
      * @since 2025.01.31
      */
     @DeleteMapping("/mine")
-    public GenericResponse mine(@RequestBody @Valid QuitReqBody reqBody) {
+    public GenericResponse mine(@RequestBody @Valid PasswordDto passwordDto) {
         Member member = checkAuthMember();
 
-        if (!reqBody.password.equals(member.getPassword()))
+        if (!passwordDto.getPassword().equals(member.getPassword()))
             throw new MemberException(INCORRECT_AUTHORIZED);
 
         memberService.delete(member);
