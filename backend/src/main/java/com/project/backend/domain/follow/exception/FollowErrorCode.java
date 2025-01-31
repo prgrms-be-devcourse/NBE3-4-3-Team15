d5eb91986ab5e4f5ceb8d632b56eaf1035e73faa
wjwj910 <wjwj910@gmail.com>
2025-01-31 11:43:24 +0900

    refactor: errorcode 수정

--- backend/src/main/java/com/project/backend/domain/follow/exception/FollowErrorCode.java
+++ backend/src/main/java/com/project/backend/domain/follow/exception/FollowErrorCode.java
@@ -9,8 +9,7 @@ import org.springframework.http.HttpStatus;
  */
 public enum FollowErrorCode {
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "404-1", "존재하지 않는 회원입니다."),
-    ALREADY_FOLLOWING(HttpStatus.BAD_REQUEST, "400-2", "이미 팔로우한 회원입니다."),
-    CAN_NOT_FOLLOW_MYSELF(HttpStatus.BAD_REQUEST, "400-3", "자기 자신을 팔로우할 수 없습니다.");
+    CAN_NOT_FOLLOW_MYSELF(HttpStatus.BAD_REQUEST, "400-1", "자기 자신을 팔로우할 수 없습니다.");
 
     final HttpStatus status;
     final String code;
