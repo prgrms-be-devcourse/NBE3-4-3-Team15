ff9de193abeef72e202d01f861be8636963daa1a
janjan <97529177+zxc8661@users.noreply.github.com>
2025-02-03 15:00:46 +0900

    feat: 리뷰 범용 에러처리 추가(ReviewerrorCode, ReviewExcetion, ReviewexceptionHandler)

---
+++ backend/src/main/java/com/project/backend/domain/review/exception/ReviewErrorCode.java
@@ -0,0 +1,26 @@
+package com.project.backend.domain.review.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ReviewErrorCode {
+
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW_NOT_FOUND", "리뷰를 찾을 수 없습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT_NOT_FOUND", "리뷰 코멘트를 찾을 수 없습니다."),
+    DUPLICATE_REVIEW(HttpStatus.CONFLICT, "DUPLICATE_REVIEW", "이미 해당 리뷰가 존재합니다."),
+    INVALID_REVIEW_CONTENT(HttpStatus.BAD_REQUEST, "INVALID_REVIEW_CONTENT", "리뷰 내용이 유효하지 않습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND","해당 맴버를 찾을 수 없습니다.");
+
+
+    private final HttpStatus status;
+    private final String errorCode;
+    private final String message;
+
+    ReviewErrorCode(HttpStatus status, String errorCode, String message){
+        this.status = status;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+}
