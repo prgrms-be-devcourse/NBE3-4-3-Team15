d76d1a4e05af2a7f6e9bcb67dc1c99351ec2a884
kk0506014 <kk0506014@gmail.com>
2025-02-07 01:12:15 +0900

    refact : BookController 세션 -> 토큰 관리 수정

--- backend/src/main/java/com/project/backend/domain/book/controller/BookController.java
+++ backend/src/main/java/com/project/backend/domain/book/controller/BookController.java
@@ -37,7 +37,7 @@ public class BookController {
      * 작가 검색, 제목 검색 기능
      *
      * @param -- query(검색어), searchBy(title = 제목검색, author = 작가검색) --
-     * @header -- X-Session-Id (개인별 세션 ID) --
+     * @header -- Authorization --
      * @return -- GenericResponse<List<BookDTO>> --
      * @author -- 정재익 --
      * @since -- 2월 5일 --
@@ -46,8 +46,8 @@ public class BookController {
     @Operation(summary = "도서 검색")
     public GenericResponse<List<BookDTO>> searchBooks(@RequestParam(name = "query") String query,
                                                       @RequestParam(name = "searchBy", defaultValue = "title") String searchBy,
-                                                      @RequestHeader(name = "X-Session-Id") String sessionId) {
-        return GenericResponse.of(bookService.searchBooks(query, searchBy.equalsIgnoreCase("author"), sessionId));
+                                                      @RequestHeader(name = "Authorization") String token) {
+        return GenericResponse.of(bookService.searchBooks(query, searchBy.equalsIgnoreCase("author"), token));
     }
 
     /**
@@ -55,47 +55,47 @@ public class BookController {
      * 책의 상세 정보 조회
      *
      * @param -- isbn --
-     * @header -- X-Session-Id (개인별 세션 ID) --
+     * @header -- Authorization --
      * @return -- GenericResponse<BookDTO> --
      * @author -- 정재익 --
      * @since -- 2월 5일 --
      */
     @GetMapping("/{isbn}")
-    @Operation(summary = "도서 상세 검색")
+    @Operation(summary = "도서 상세 조회")
     public GenericResponse<BookDTO> searchBookDetail(@PathVariable(name = "isbn") String isbn,
-                                                     @RequestHeader(name = "X-Session-Id") String sessionId) {
-        return GenericResponse.of(bookService.searchBookDetail(isbn, sessionId));
+                                                     @RequestHeader(name = "Authorization") String token) {
+        return GenericResponse.of(bookService.searchBookDetail(isbn, token));
     }
 
-    /**
-     * 도서 찜하기,취소하기 기능
-     * 로그인한 사용자의 정보를 @AuthenticationPrincipal을 통해 가져와 FavoriteRepository를 이용하여 찜하거나 찜 취소
-     * 달라진 도서별 찜 개수를 BookRepository에 반영
-     *
-     * @param -- FavoriteDTO --
-     * @param -- userDetails 로그인한 사용자 정보 --
-     * @return -- GenericResponse<String>
-     * @author -- 정재익 --
-     * @since -- 2월 3일 --
-     */
-    @PostMapping("/{id}/favorite")
-    @Operation(summary = "도서 찜 하기")
-    public GenericResponse<String> favoriteBook(@Valid @RequestBody FavoriteDTO favoriteDto, @AuthenticationPrincipal UserDetails userDetails) {
-        return bookService.favoriteBook(favoriteDto, userDetails);
-    }
-
-    /**
-     * -- 찜한 책 목록을 확인하는 메소드 --
-     * 로그인한 사용자의 정보를 @AuthenticationPrincipal을 통해 가져와 favoriteRepository에서 찜한 책 목록 조회
-     *
-     * @param -- userDetails 로그인한 사용자 정보 --
-     * @return -- GenericResponse<List<BookSimpleDTO>> --
-     * @author -- 정재익 --
-     * @since -- 2월 3일 --
-     */
-    @GetMapping("/favorite")
-    @Operation(summary = "도서 찜 목록")
-    public GenericResponse<List<BookDTO>> searchFavoriteBooks(@AuthenticationPrincipal UserDetails userDetails) {
-        return GenericResponse.of(bookService.searchFavoriteBooks(userDetails));
-    }
+//    /**
+//     * 도서 찜하기,취소하기 기능
+//     * 로그인한 사용자의 정보를 @AuthenticationPrincipal을 통해 가져와 FavoriteRepository를 이용하여 찜하거나 찜 취소
+//     * 달라진 도서별 찜 개수를 BookRepository에 반영
+//     *
+//     * @param -- FavoriteDTO --
+//     * @param -- userDetails 로그인한 사용자 정보 --
+//     * @return -- GenericResponse<String>
+//     * @author -- 정재익 --
+//     * @since -- 2월 3일 --
+//     */
+//    @PostMapping("/{id}/favorite")
+//    @Operation(summary = "도서 찜 하기")
+//    public GenericResponse<String> favoriteBook(@Valid @RequestBody FavoriteDTO favoriteDto, @AuthenticationPrincipal UserDetails userDetails) {
+//        return bookService.favoriteBook(favoriteDto, userDetails);
+//    }
+//
+//    /**
+//     * -- 찜한 책 목록을 확인하는 메소드 --
+//     * 로그인한 사용자의 정보를 @AuthenticationPrincipal을 통해 가져와 favoriteRepository에서 찜한 책 목록 조회
+//     *
+//     * @param -- userDetails 로그인한 사용자 정보 --
+//     * @return -- GenericResponse<List<BookSimpleDTO>> --
+//     * @author -- 정재익 --
+//     * @since -- 2월 3일 --
+//     */
+//    @GetMapping("/favorite")
+//    @Operation(summary = "도서 찜 목록")
+//    public GenericResponse<List<BookDTO>> searchFavoriteBooks(@AuthenticationPrincipal UserDetails userDetails) {
+//        return GenericResponse.of(bookService.searchFavoriteBooks(userDetails));
+//    }
 }
