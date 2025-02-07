4703f75ee362c72f90b5b4be013856cc351cf834
kk0506014 <kk0506014@gmail.com>
2025-02-07 17:58:46 +0900

    feat : BookRepository favoriteCount Update Query 생성

--- backend/src/main/java/com/project/backend/domain/book/repository/BookRepository.java
+++ backend/src/main/java/com/project/backend/domain/book/repository/BookRepository.java
@@ -3,6 +3,9 @@ package com.project.backend.domain.book.repository;
 import com.project.backend.domain.book.entity.Book;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -17,4 +20,12 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
 
     List<Book> findAll(Sort sort);
+
+    @Modifying
+    @Query("UPDATE Book b SET b.favoriteCount = b.favoriteCount + 1 WHERE b.isbn = :isbn")
+    void incrementFavoriteCount(@Param("isbn") String isbn);
+
+    @Modifying
+    @Query("UPDATE Book b SET b.favoriteCount = b.favoriteCount - 1 WHERE b.isbn = :isbn")
+    void decrementFavoriteCount(@Param("isbn") String isbn);
 }
