d3dd67c5c92ca7e53d9f80a7b2f824718d0a0b8f
wjwj910 <wjwj910@gmail.com>
2025-02-07 12:11:16 +0900

    refactor: FollowController, 리팩토링

--- backend/src/main/java/com/project/backend/domain/follow/controller/FollowController.java
+++ backend/src/main/java/com/project/backend/domain/follow/controller/FollowController.java
@@ -1,11 +1,12 @@
 package com.project.backend.domain.follow.controller;
 
-import com.project.backend.domain.follow.dto.FollowRequestDto;
 import com.project.backend.domain.follow.dto.FollowResponseDto;
 import com.project.backend.domain.follow.service.FollowService;
+import com.project.backend.global.authority.CustomUserDetails;
 import com.project.backend.global.response.GenericResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,7 +14,7 @@ import java.util.List;
 /**
  * 팔로우 기능을 처리하는 컨트롤러
  * author: 이원재
- * since: 2025.01.31
+ * since: 2025.02.07
  */
 @RestController
 @RequestMapping("/members")
@@ -23,37 +24,45 @@ public class FollowController {
     private final FollowService followService;
 
     /**
+     * @param username 팔로우/언팔로우를 하고자 하는 회원 username
+     * @param userDetails 로그인한 회원의 정보
      * 팔로우 또는 언팔로우
-     * @param memberId 팔로우를 요청하는 회원 ID
-     * @param requestDto 팔로우 대상 회원 ID
-     * @return 팔로우 성공 여부
+     * @return 팔로우/언팔로우 성공 여부
      */
-    @PostMapping("/{memberId}/follow")
-    public ResponseEntity<GenericResponse<Void>> followOrUnfollow(@PathVariable String memberId,
-                                                                  @RequestBody FollowRequestDto requestDto) {
-        followService.followOrUnfollow(memberId, requestDto);
-        return ResponseEntity.ok(GenericResponse.of());
+    @PostMapping("/{id}/follow")
+    public ResponseEntity<GenericResponse<String>> followOrUnfollow(
+            @PathVariable("id") String username,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        String message = followService.followOrUnfollow(userDetails.getUsername(), username);
+
+        return ResponseEntity.ok(GenericResponse.of(message));
     }
 
     /**
      * 사용자의 팔로잉 목록 조회
-     * @param memberId 조회할 회원 ID
-     * @return 팔로잉 목록
+     * (팔로잉 목록 = 내가 팔로우 한 사람의 목록)
+     * @param username 요청하는 회원 username
+     * @return 회원 ID에 해당하는 팔로잉 목록
      */
-    @GetMapping("/{memberId}/followings")
-    public ResponseEntity<GenericResponse<List<FollowResponseDto>>> getFollowings(@PathVariable String memberId) {
-        List<FollowResponseDto> followings = followService.getFollowings(memberId);
-        return ResponseEntity.ok(GenericResponse.of(followings));
+    @GetMapping("/{id}/followings")
+    public ResponseEntity<GenericResponse<List<FollowResponseDto>>> getFollowings(
+            @PathVariable("id") String username) {
+        List<FollowResponseDto> followings = followService.getFollowings(username);
+
+        return ResponseEntity.ok(GenericResponse.of(followings, "팔로잉 목록 조회 성공"));
     }
 
     /**
      * 사용자의 팔로워 목록 조회
-     * @param memberId 조회할 회원 ID
-     * @return 팔로워 목록
+     * (팔로워 목록 = 나를 팔로우 한 사람의 목록)
+     * @param username 요청하는 회원 username
+     * @return 회원 ID에 해당하는 팔로워 목록
      */
-    @GetMapping("/{memberId}/followers")
-    public ResponseEntity<GenericResponse<List<FollowResponseDto>>> getFollowers(@PathVariable String memberId) {
-        List<FollowResponseDto> followers = followService.getFollowers(memberId);
-        return ResponseEntity.ok(GenericResponse.of(followers));
+    @GetMapping("/{id}/followers")
+    public ResponseEntity<GenericResponse<List<FollowResponseDto>>> getFollowers(
+            @PathVariable("id") String username) {
+        List<FollowResponseDto> followers = followService.getFollowers(username);
+
+        return ResponseEntity.ok(GenericResponse.of(followers,"팔로워 목록 조회 성공"));
     }
 }
