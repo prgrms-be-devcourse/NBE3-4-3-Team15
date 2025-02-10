a8a2433d2fa54de760833d9c1571bd1b0b2df5f5
kk0506014 <kk0506014@gmail.com>
2025-02-11 01:37:26 +0900

    feat : FavoriteRepository deleteByMemberId, findBookIdsByMemberId 생성 / 회원 탈퇴시 찜 관련 도서 UPDATE 구현

--- backend/src/main/java/com/project/backend/domain/book/repository/FavoriteRepository.java
+++ backend/src/main/java/com/project/backend/domain/book/repository/FavoriteRepository.java
@@ -3,12 +3,16 @@ package com.project.backend.domain.book.repository;
 import com.project.backend.domain.book.dto.BookDTO;
 import com.project.backend.domain.book.entity.Favorite;
 import com.project.backend.domain.book.key.FavoriteId;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * -- 찜 저장소 --
@@ -23,4 +27,27 @@ public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> 
             "JOIN f.book b " +
             "WHERE f.member.id = :memberId")
     Page<BookDTO> findFavoriteBooksByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+
+    /**
+     * -- 특정 회원의 찜 목록 삭제 --
+     *
+     * @param memberId 회원 ID
+     * @author -- 정재익 --
+     * @since -- 2월 10일 --
+     */
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Favorite f WHERE f.member.id = :memberId")
+    void deleteByMemberId(@Param("memberId") Long memberId);
+
+    /**
+     * -- 특정 회원이 찜한 도서 ID 목록 조회 --
+     *
+     * @param memberId 회원 ID
+     * @return 회원이 찜한 도서 ID 목록
+     * @author -- 김남우 --
+     * @since -- 2월 10일 --
+     */
+    @Query("SELECT f.book.id FROM Favorite f WHERE f.member.id = :memberId")
+    List<Long> findBookIdsByMemberId(@Param("memberId") Long memberId);
 }
