5f4815b8e44924fcfbacbe76ea01916dfe66228b
kk0506014 <kk0506014@gmail.com>
2025-02-10 17:22:57 +0900

    refact : List -> Page 변경

--- backend/src/main/java/com/project/backend/domain/book/repository/FavoriteRepository.java
+++ backend/src/main/java/com/project/backend/domain/book/repository/FavoriteRepository.java
@@ -3,12 +3,12 @@ package com.project.backend.domain.book.repository;
 import com.project.backend.domain.book.dto.BookDTO;
 import com.project.backend.domain.book.entity.Favorite;
 import com.project.backend.domain.book.key.FavoriteId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * -- 찜 저장소 --
@@ -22,5 +22,5 @@ public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> 
             "FROM Favorite f " +
             "JOIN f.book b " +
             "WHERE f.member.id = :memberId")
-    List<BookDTO> findFavoriteBooksByMemberId(@Param("memberId") Long memberId);
+    Page<BookDTO> findFavoriteBooksByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 }
