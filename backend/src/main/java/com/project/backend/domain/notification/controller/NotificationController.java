3a7abed56857a0771b889edb31c3c2ce66e8ec72
janjan <97529177+zxc8661@users.noreply.github.com>
2025-02-10 10:11:38 +0900

    refactor: notificationController ResponseEntity 추가

--- backend/src/main/java/com/project/backend/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/project/backend/domain/notification/controller/NotificationController.java
@@ -5,6 +5,7 @@ import com.project.backend.domain.notification.dto.NotificationDTO;
 import com.project.backend.domain.notification.service.NotificationService;
 import com.project.backend.global.response.GenericResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,67 +25,67 @@ public class NotificationController {
     /**
      * 알림 생성
      * @param notificationDTO
-     * @return GenericResponse<NotificationDTO>
+     * @return ResponseEntity<GenericResponse<NotificationDTO>>
      *
      * @author 이광석
      * @since  25.02.06
      */
     @PostMapping
-    public GenericResponse<NotificationDTO> createNotification(@RequestBody NotificationDTO notificationDTO){
+    public ResponseEntity<GenericResponse<NotificationDTO>> createNotification(@RequestBody NotificationDTO notificationDTO){
         NotificationDTO newNotificationDTO = notificationService.create(notificationDTO);
-        return GenericResponse.of(
+        return ResponseEntity.ok(GenericResponse.of(
                 newNotificationDTO,
                 "알람 생성 성공"
-        );
+        ));
 
     }
 
     /**
      * 특정 유저 알람 조회
      * @param memberId
-     * @return GenericResponse<List<NotificationDTO>>
+     * @return ResponseEntity<GenericResponse<List<NotificationDTO>>>
      *
      * @author 이광석
      * @since 25.02.06
      */
     @GetMapping("/{memberId}")
-    public GenericResponse<List<NotificationDTO>> getUserIdNotification(@PathVariable("memberId") Long memberId ){
+    public ResponseEntity<GenericResponse<List<NotificationDTO>>> getUserIdNotification(@PathVariable("memberId") Long memberId ){
         List<NotificationDTO> notificationDTOS = notificationService.findByUser(memberId);
-        return GenericResponse.of(
+        return ResponseEntity.ok(GenericResponse.of(
                 notificationDTOS,
                 "알림 조회 성공"
-        );
+        ));
     }
 
     /**
      * 알림 읽음 상태 변경
      * @param notificationId
-     * @return GenericResponse<String>
+     * @return ResponseEntity<GenericResponse<String>>
      *
      * @author 이광석
      * @since 25.02.06
      */
     @PutMapping("/{notificationId}")
-    public GenericResponse<String> notificationCheck(@PathVariable("notificationId") Long notificationId ){
+    public ResponseEntity<GenericResponse<String>> notificationCheck(@PathVariable("notificationId") Long notificationId ){
         notificationService.notificationCheck(notificationId);
-        return GenericResponse.of(
+        return ResponseEntity.ok(GenericResponse.of(
                 "변경 성공"
-        );
+        ));
     }
 
     /**
      * 알림 삭제
      * @param notificationId
-     * @return GenericResponse<String>
+     * @return ResponseEntity<GenericResponse<String>>
      *
      * @author 이광석
      * @since
      */
     @DeleteMapping("/{notificationId}")
-    public GenericResponse<String> notificationDelete(@PathVariable("notificationId") Long notificationId){
+    public ResponseEntity<GenericResponse<String>> notificationDelete(@PathVariable("notificationId") Long notificationId){
         notificationService.notificationDelete(notificationId);
-        return GenericResponse.of(
+        return ResponseEntity.ok(GenericResponse.of(
                 "삭제 성공"
-        );
+        ));
     }
 }
