25a7841042a1bd2ed868c974ea4475fa4a687b5f
kk0506014 <kk0506014@gmail.com>
2025-03-03 18:27:06 +0900

    refactor : FavoriteRepository / import 수정

--- backend/src/main/java/com/project/backend/domain/book/repository/FavoriteRepository.kt
+++ backend/src/main/java/com/project/backend/domain/book/repository/FavoriteRepository.kt
@@ -1,18 +1,17 @@
 package com.project.backend.domain.book.repository;
 
-import com.project.backend.domain.book.dto.BookDTO;
-import com.project.backend.domain.book.entity.Favorite;
-import com.project.backend.domain.book.key.FavoriteId;
-import jakarta.transaction.Transactional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
+import com.project.backend.domain.book.dto.BookDTO
+import com.project.backend.domain.book.entity.Favorite
+import com.project.backend.domain.book.key.FavoriteId
+import jakarta.transaction.Transactional
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import java.util.List
 
 /**
  * -- 찜 저장소 --
