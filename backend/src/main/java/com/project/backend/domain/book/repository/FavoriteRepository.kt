683a7f983949083a7db525b71db8c8a889fbb8f6
kk0506014 <kk0506014@gmail.com>
2025-03-03 18:27:22 +0900

    refactor : FavoriteRepository / 주석 수정

--- backend/src/main/java/com/project/backend/domain/book/repository/FavoriteRepository.kt
+++ backend/src/main/java/com/project/backend/domain/book/repository/FavoriteRepository.kt
@@ -16,8 +16,8 @@ import java.util.List
 /**
  * -- 찜 저장소 --
  *
- * @author -- 정재익 --
- * @since -- 1월 27일 --
+ * @author -- 김남우 --
+ * @since -- 3월 3일 --
  */
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
@@ -28,7 +28,7 @@ public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> 
      * @param pageable 페이징 처리 객체
      * @return 회원이 찜한 도서 목록
      * @author -- 김남우 --
-     * @since -- 2월 10일 --
+     * @since -- 3월 3일 --
      */
     @Query("SELECT new com.project.backend.domain.book.dto.BookDTO(b.id, b.title, b.author, b.description, b.image, b.isbn, b.favoriteCount) " +
             "FROM Favorite f " +
@@ -40,8 +40,8 @@ public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> 
      * -- 특정 회원의 찜 목록 삭제 --
      *
      * @param memberId 회원 ID
-     * @author -- 정재익 --
-     * @since -- 2월 10일 --
+     * @author -- 김남우 --
+     * @since -- 3월 3일 --
      */
     @Modifying
     @Transactional
@@ -54,7 +54,7 @@ public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> 
      * @param memberId 회원 ID
      * @return 회원이 찜한 도서 ID 목록
      * @author -- 김남우 --
-     * @since -- 2월 10일 --
+     * @since -- 3월 3일 --
      */
     @Query("SELECT f.book.id FROM Favorite f WHERE f.member.id = :memberId")
     List<Long> findBookIdsByMemberId(@Param("memberId") Long memberId);
