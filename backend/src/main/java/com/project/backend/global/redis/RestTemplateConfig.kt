5a9744d482c8d4cdf0995d6a364456d12e64c7fb
kk0506014 <kk0506014@gmail.com>
2025-03-05 17:54:38 +0900

    refactor : RestTemplateConfig / golbal으로 이동

--- backend/src/main/java/com/project/backend/global/redis/RestTemplateConfig.kt
+++ backend/src/main/java/com/project/backend/global/redis/RestTemplateConfig.kt
@@ -1,4 +1,4 @@
-package com.project.backend.domain.book.config
+package com.project.backend.global.redis
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
