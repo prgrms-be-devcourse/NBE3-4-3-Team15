735496d7acb8282145afe8d3d50a102e17bcce1e
janjan <97529177+zxc8661@users.noreply.github.com>
2025-03-05 16:10:11 +0900

    reddis 추가

---
+++ backend/src/main/java/com/project/backend/global/redis/RedisService.java
@@ -0,0 +1,53 @@
+package com.project.backend.global.redis;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * Redis 데이터 저장,삭제, 검색 Service
+ *
+ * @author 이광석
+ * @since 25.02.26
+ */
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * Redis 데이터 저장
+     * @param key
+     * @param value
+     *
+     *@author 이광석
+     *@since 25.02.26
+     */
+    public void saveData(String key,String value){
+        redisTemplate.opsForValue().set(key,value);
+    }
+
+    /**
+     * Redis 데이터 검색
+     * @param key
+     * @return value
+     *
+     *@author 이광석
+     *@since 25.02.26
+     */
+    public String getData(String key){
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * Redis 데이터 삭제
+     * @param key
+     *
+     * @author 이광석
+     * @since 25.02.26
+     */
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+}
