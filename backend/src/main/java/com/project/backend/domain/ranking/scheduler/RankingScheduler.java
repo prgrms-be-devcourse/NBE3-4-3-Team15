63635ff3646c9c4a242ae94e8f1fb9c2bdef950a
kk0506014 <kk0506014@gmail.com>
2025-03-07 14:30:32 +0900

    feature : RankingScheduler / 랭킹 스케줄러 생성

---
+++ backend/src/main/java/com/project/backend/domain/ranking/scheduler/RankingScheduler.java
@@ -0,0 +1,31 @@
+package com.project.backend.domain.ranking.scheduler;
+
+import com.project.backend.domain.ranking.service.RankingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * -- 랭킹 스케줄러 --
+ *
+ * @author -- 김남우 --
+ * @since -- 3월 4일 --
+ */
+@Component
+@RequiredArgsConstructor
+public class RankingScheduler {
+
+    private final RankingService rankingService;
+
+    @Scheduled(cron = "0 * * * * *") // 매시간 0분 0초에 실행
+    public void updateWeeklyRanking(){
+        LocalDateTime start = LocalDateTime.now().minusWeeks(1); // 1주일 전부터
+        LocalDateTime end = LocalDateTime.now(); // 현재 시간까지
+
+        System.out.println("주간 랭킹 업데이트 : " + end);
+
+        rankingService.updateWeeklyBooksRanking(start, end);
+    }
+}
