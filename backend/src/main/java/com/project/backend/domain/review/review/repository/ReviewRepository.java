2d7c11a19081a69815bba49bd82334a529a9ea3a
kk0506014 <kk0506014@gmail.com>
2025-03-07 14:24:40 +0900

    feature : ReviewRepository / countReviewsByBookIdAndDateTime 생성

--- backend/src/main/java/com/project/backend/domain/review/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/project/backend/domain/review/review/repository/ReviewRepository.java
@@ -5,7 +5,10 @@ import com.project.backend.domain.review.review.reviewDTO.ReviewsDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
