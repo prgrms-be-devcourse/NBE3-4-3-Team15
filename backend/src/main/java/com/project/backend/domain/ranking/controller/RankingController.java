70c7d0d872a9613fd6552e0050d6ea12188c9bcf
kk0506014 <kk0506014@gmail.com>
2025-03-11 09:28:41 +0900

    refactor : RankingController.java / getWeeklyRanking -> getWeeklyBookRanking 메서드 명 변경

--- backend/src/main/java/com/project/backend/domain/ranking/controller/RankingController.java
+++ backend/src/main/java/com/project/backend/domain/ranking/controller/RankingController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,13 +26,12 @@ import java.util.Map;
 @SecurityRequirement(name = "bearerAuth")
 public class RankingController {
 
-    private final RedisTemplate<String, Object> redisTemplate;
     private final RankingService rankingService;
 
     @GetMapping("/weekly/book")
     @Operation(summary = "주간 인기 도서 랭킹")
-    public ResponseEntity<List<Map<String, Object>>> getWeeklyRanking() {
-        List<Map<String, Object>> weeklyRanking = rankingService.getWeeklyRanking();
-        return ResponseEntity.ok(weeklyRanking);
+    public ResponseEntity<List<Map<String, Object>>> getWeeklyBookRanking() {
+        List<Map<String, Object>> weeklyBookRanking = rankingService.getWeeklyBookRanking();
+        return ResponseEntity.ok(weeklyBookRanking);
     }
 }
