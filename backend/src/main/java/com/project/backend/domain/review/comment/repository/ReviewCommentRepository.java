31d871eaca63fb234f53b05eaff38cb332da2b3f
kk0506014 <kk0506014@gmail.com>
2025-03-11 15:09:15 +0900

    feature : ReviewCommentRepository.java / 주석 생성

--- backend/src/main/java/com/project/backend/domain/review/comment/repository/ReviewCommentRepository.java
+++ backend/src/main/java/com/project/backend/domain/review/comment/repository/ReviewCommentRepository.java
@@ -25,6 +25,16 @@ public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Lo
 
     List<ReviewCommentDto> findAllByUserId(Long userId);
 
+    /**
+     * 특정 기간 동안 작성된 리뷰 댓글 수를 리뷰별로 집계하여 조회하는 메서드
+     *
+     * @param start 조회 시작 날짜
+     * @param end 조회 종료 날짜
+     * @return 각 리뷰의 ID와 해당 기간 동안 작성된 댓글 개수를 포함한 리스트
+     *
+     * @author 김남우
+     * @since 2025.03.10
+     */
     @Query("SELECT rc.review.id, COUNT(rc.id) FROM ReviewComment rc WHERE rc.createdAt BETWEEN :start AND :end GROUP BY rc.review.id")
     List<Object[]> findReviewCommentCounts(
             @Param("start") LocalDateTime start,
