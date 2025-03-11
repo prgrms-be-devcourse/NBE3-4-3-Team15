3a5ed37a78aaba11c2aeb8f680361702f9a1e6d4
kk0506014 <kk0506014@gmail.com>
2025-03-11 15:09:38 +0900

    feature : ReviewRepository.java / 주석 생성

--- backend/src/main/java/com/project/backend/domain/review/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/project/backend/domain/review/review/repository/ReviewRepository.java
@@ -24,6 +24,16 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
 
     List<Review> findAllByUserIdOrderByBookIdDesc(Long userId);
 
+    /**
+     * 특정 기간 동안 작성된 리뷰 수를 도서별로 집계하여 조회하는 메서드
+     *
+     * @param start 조회 시작 날짜
+     * @param end 조회 종료 날짜
+     * @return 각 도서의 ID와 해당 기간 동안 작성된 리뷰 수를 포함한 리스트
+     *
+     * @author 김남우
+     * @since 2025.03.06
+     */
     @Query("SELECT r.bookId, COUNT(r.id) FROM Review r WHERE r.createdAt BETWEEN :start AND :end GROUP BY r.bookId")
     List<Object[]> findReviewCounts(
             @Param("start") LocalDateTime start,
