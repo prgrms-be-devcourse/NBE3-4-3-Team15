80d0eedcab6dd4b197698a6b757559f58c4ee50d
kk0506014 <kk0506014@gmail.com>
2025-03-11 10:17:53 +0900

    feature : RankingScheduler.java / updateWeeklyReviewsRanking 생성

--- backend/src/main/java/com/project/backend/domain/ranking/scheduler/RankingScheduler.java
+++ backend/src/main/java/com/project/backend/domain/ranking/scheduler/RankingScheduler.java
@@ -19,7 +19,8 @@ public class RankingScheduler {
 
     private final RankingService rankingService;
 
-    @Scheduled(cron = "0 * * * * *") // 매시간 0분 0초에 실행
+//    @Scheduled(cron = "0 0 * * * *")  // 매시간 0분 0초에 실행
+    @Scheduled(cron = "0 * * * * *")
     public void updateWeeklyRanking(){
         LocalDateTime start = LocalDateTime.now().minusWeeks(1); // 1주일 전부터
         LocalDateTime end = LocalDateTime.now(); // 현재 시간까지
@@ -27,5 +28,6 @@ public class RankingScheduler {
         System.out.println("주간 랭킹 업데이트 : " + end);
 
         rankingService.updateWeeklyBooksRanking(start, end);
+        rankingService.updateWeeklyReviewsRanking(start, end);
     }
 }
