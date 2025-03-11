5f794cef86748e7b08e7dbf898221e3bb9d30082
kk0506014 <kk0506014@gmail.com>
2025-03-11 15:08:49 +0900

    feature : RankingController.java / 주석 생성

--- backend/src/main/java/com/project/backend/domain/ranking/controller/RankingController.java
+++ backend/src/main/java/com/project/backend/domain/ranking/controller/RankingController.java
@@ -18,7 +18,7 @@ import java.util.Map;
  * -- 랭킹 컨트롤러 --
  *
  * @author -- 김남우 --
- * @since -- 3월 4일 --
+ * @since -- 2025.03.04 --
  */
 @Tag(name = "Ranking", description = "랭킹 컨트롤러")
 @RestController
@@ -29,24 +29,57 @@ public class RankingController {
 
     private final RankingService rankingService;
 
+    /**
+     * 주간 인기 도서 랭킹 조회 API
+     *
+     * @return 주간 인기 도서 목록
+     *
+     * @author 김남우
+     * @since 2025.03.05
+     */
     @GetMapping("/weekly/book")
     @Operation(summary = "주간 인기 도서 랭킹")
     public ResponseEntity<List<Map<String, Object>>> getWeeklyBookRanking() {
         return getRankingResponse(RankingType.WEEKLY_BOOKS);
     }
 
+    /**
+     * 주간 인기 리뷰 랭킹 조회 API
+     *
+     * @return 주간 인기 리뷰 목록
+     *
+     * @author 김남우
+     * @since 2025.03.09
+     */
     @GetMapping("/weekly/review")
     @Operation(summary = "주간 인기 리뷰 랭킹")
     public ResponseEntity<List<Map<String, Object>>> getWeeklyReviewRanking() {
         return getRankingResponse(RankingType.WEEKLY_REVIEWS);
     }
 
+    /**
+     * 일간 인기 리뷰 랭킹 조회 API
+     *
+     * @return 일간 인기 리뷰 목록
+     *
+     * @author 김남우
+     * @since 2025.03.10
+     */
     @GetMapping("/daily/review")
     @Operation(summary = "인기 급상승 리뷰 랭킹")
     public ResponseEntity<List<Map<String, Object>>> getDailyReviewsRanking() {
         return getRankingResponse(RankingType.DAILY_REVIEWS);
     }
 
+    /**
+     * 지정된 랭킹 유형에 따라 랭킹 데이터를 조회하는 내부 메서드
+     *
+     * @param rankingType 조회할 랭킹 유형
+     * @return 랭킹 리스트
+     *
+     * @author 김남우
+     * @since 2025.03.11
+     */
     private ResponseEntity<List<Map<String, Object>>> getRankingResponse(RankingType rankingType) {
         return ResponseEntity.ok(rankingService.getRanking(rankingType));
     }
