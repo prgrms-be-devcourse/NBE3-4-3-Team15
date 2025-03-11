7be8eeefb46782f66e6be0d20b59eb1f9153dd69
kk0506014 <kk0506014@gmail.com>
2025-03-11 15:09:08 +0900

    feature : RankingType.java / 주석 생성

--- backend/src/main/java/com/project/backend/domain/ranking/common/RankingType.java
+++ backend/src/main/java/com/project/backend/domain/ranking/common/RankingType.java
@@ -1,5 +1,12 @@
 package com.project.backend.domain.ranking.common;
 
+/**
+ * 랭킹 유형을 정의하는 열거형 (Enum)
+ * 각 랭킹 유형은 Redis에서 사용하는 고유한 키 값을 가짐.
+ *
+ * @author 김남우
+ * @since 2025.03.11
+ */
 public enum RankingType {
     WEEKLY_BOOKS("weekly_books_ranking"),
     WEEKLY_REVIEWS("weekly_reviews_ranking"),
