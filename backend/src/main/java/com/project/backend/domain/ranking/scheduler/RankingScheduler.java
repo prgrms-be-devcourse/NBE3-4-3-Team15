ed81b4fde0e764c2c3c5e8d1f533ec0f14ccb07d
kk0506014 <kk0506014@gmail.com>
2025-03-11 12:03:51 +0900

    refactor : RankingScheduler.java / rankingType 생성으로 인한 updateRankingByPeriod 생성

--- backend/src/main/java/com/project/backend/domain/ranking/scheduler/RankingScheduler.java
+++ backend/src/main/java/com/project/backend/domain/ranking/scheduler/RankingScheduler.java
@@ -1,5 +1,6 @@
 package com.project.backend.domain.ranking.scheduler;
 
+import com.project.backend.domain.ranking.common.RankingType;
 import com.project.backend.domain.ranking.service.RankingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -22,23 +23,26 @@ public class RankingScheduler {
 //    @Scheduled(cron = "0 0 * * * *")  // 매시간 0분 0초에 실행
     @Scheduled(cron = "0 * * * * *")
     public void updateWeeklyRanking(){
-        LocalDateTime start = LocalDateTime.now().minusWeeks(1); // 1주일 전부터
-        LocalDateTime end = LocalDateTime.now(); // 현재 시간까지
-
-        System.out.println("주간 랭킹 업데이트 : " + end);
-
-        rankingService.updateWeeklyBooksRanking(start, end);
-        rankingService.updateWeeklyReviewsRanking(start, end);
+        updateRankingByPeriod(1, "주간");
     }
 
-//    @Scheduled(cron = "0 */10 * * * *") // 매시간 10분 마다 실행
+    //    @Scheduled(cron = "0 */10 * * * *") // 매시간 10분 마다 실행
     @Scheduled(cron = "0 * * * * *")
-    public void updatedaliyRanking(){
-        LocalDateTime start = LocalDateTime.now().minusDays(1); // 1일 전부터
-        LocalDateTime end = LocalDateTime.now(); // 현재 시간까지
+    public void updateDailyRanking(){
+        updateRankingByPeriod(0, "일간");
+    }
 
-        System.out.println("일간 랭킹 업데이트 : " + end);
+    private void updateRankingByPeriod(int weeksOrDays, String periodType) {
+        LocalDateTime start = weeksOrDays > 0 ? LocalDateTime.now().minusWeeks(weeksOrDays) : LocalDateTime.now().minusDays(1);
+        LocalDateTime end = LocalDateTime.now();
 
-        rankingService.updateDailyReviewsRanking(start, end);
+        System.out.println(periodType + " 랭킹 업데이트 : " + end);
+
+        if (weeksOrDays > 0) { // 주간 랭킹
+            rankingService.updateRanking(RankingType.WEEKLY_BOOKS, start, end);
+            rankingService.updateRanking(RankingType.WEEKLY_REVIEWS, start, end);
+        } else { // 일간 랭킹
+            rankingService.updateRanking(RankingType.DAILY_REVIEWS, start, end);
+        }
     }
 }
